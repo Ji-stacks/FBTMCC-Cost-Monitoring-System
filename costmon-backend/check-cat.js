3262cabc98c88f7c00b093ce41165f0b16065c82
Ferkeem Mamayson <Ferkeem Mamayson@YSOKI-NITROV>
2026-07-06 11:53:32 +0800

    feat: dynamically populate category dropdowns with custom project types

---
+++ costmon-backend/check-cat.js
@@ -0,0 +1,1 @@
+const sqlite3 = require('sqlite3').verbose(); const db = new sqlite3.Database('fbtmcc.db'); db.all('SELECT * FROM expense_categories', (err, rows) => { const fs = require('fs'); fs.writeFileSync('cats.json', JSON.stringify(rows || err, null, 2)); db.close(); });
